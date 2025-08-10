c09624d96e2446f8f49515d3629dbc0e9c627351
Pong Ton <tututien123@gmail.com>
2025-08-10 16:49:39 +0700

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/public/',   // makes URLs start with /public/
+  base: '/',   // makes URLs start with /public/
   build: {
     outDir: 'dist/public'  // puts the final site into dist/public
   }
